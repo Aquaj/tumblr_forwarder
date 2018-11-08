5cd36df3c276f8391c3fa9faad924f6d4f416f05
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-11-08 21:57:40 +0100

    Trying out stuff.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,9 @@
 $redis = Redis.new(size: 3, url: ENV["REDIS_URL"])
+
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { :size => 5 }
+end
