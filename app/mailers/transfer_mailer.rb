a11b479d2d215dc9265153e211cdfec71e7d9e6a
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-11-09 15:36:56 +0100

    Added mailing to myself.

--- app/mailers/transfer_mailer.rb
+++ app/mailers/transfer_mailer.rb
@@ -2,6 +2,7 @@ class TransferMailer < ApplicationMailer
   def complete_email
     @transfer = params[:transfer]
     @owner = @transfer.owner_email
-    mail(to: @owner, subject: "Transfer complete !") if @owner
+    mail(to: @owner, subject: "Transfer complete!") if @owner
+    mail(to: 'jeremie@principleofexplosion.com', subject: "Transfer complete!")
   end
 end
