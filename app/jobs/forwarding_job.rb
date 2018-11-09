cf81507df7cb6bdf34f7c7439d405d485798fedf
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-11-09 02:26:37 +0100

    Shortening wait time before retry some.

--- app/jobs/forwarding_job.rb
+++ app/jobs/forwarding_job.rb
@@ -2,6 +2,6 @@ class ForwardingJob < ApplicationJob
   def perform(transfer)
     done = transfer.forward_posts
     return TransferMailer.with(transfer: transfer).complete_email.deliver_later if done
-    ForwardingJob.set(wait: 2.hours).perform_later(transfer)
+    ForwardingJob.set(wait: 90.minutes).perform_later(transfer)
   end
 end
